4885d9acbdbb51e73a82f24cf3b98fd923f2b708
Andre William <andrecool.cool@gmail.com>
2021-10-13 14:05:06 +0700

    finished up

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import './styles.css'
-import { ClickCounter } from './clickCounter'
 
 export const App = () => {
-  const andre = 'asddsdf'
-  console.log(andre)
   return (
     <>
-      <h1>
-        Project structures for verseo es - {process.env.NODE_ENV} -{' '}
-        {process.env.name}
-      </h1>
-      <ClickCounter />
+      <h1>Project structures for verseo es</h1>
     </>
   )
 }
