f894260218508a91076515aeef2fca32f54afb18
Andre William <andrecool.cool@gmail.com>
2021-10-13 13:26:47 +0700

    add prettier husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './styles.css';
-import { ClickCounter } from './clickCounter';
+import './styles.css'
+import { ClickCounter } from './clickCounter'
 
 export const App = () => {
+  const andre = 'asdf'
   return (
     <>
       <h1>
-        Project structures for verseo es - {process.env.NODE_ENV} - {process.env.name}
+        Project structures for verseo es - {process.env.NODE_ENV} -{' '}
+        {process.env.name}
       </h1>
       <ClickCounter />
     </>
-  );
-};
+  )
+}
